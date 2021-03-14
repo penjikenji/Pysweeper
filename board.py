9551e870d4d4d990b2ef5d6bf224cf7b98463c2c
penjikenji <buitien28@gmail.com>
2021-03-13 19:03:38 -0500

    Moved methods from Tile and made search algorithm

--- board.py
+++ board.py
@@ -1,42 +1,123 @@
-######-TODOs-######
-# TODO line 13: Different preset grid sizes depending on difficulty? (implement a self.tileAmount)
-# TODO line 27: Use an array of objects instead of integers?
-######-------######
-
-
-
 import pygame
+from tile import Tile
 from random import randint
 
-# Static variables for color
 class Board():
     def __init__(self):
-        self.mines = 10 # Really don't need this tbh
+        self.mines = 10
         self.grid = []
+
     # A list comphrehension that makes a 9x9 2D array for the grid
-    # TODO: Different preset grid sizes depending on difficulty? (implement a self.tileAmount)
     def makeGrid(self):
-        self.grid = [[0 for x in range(10)] for x in range(10)]
+        self.grid = [[Tile() for x in range(10)] for x in range(10)]
    
     # Makes some mines
-    # TODO: depending on the size of the grid, up the amount of mines?
-    # Or just have a set amount depending the size of grid
     def makeMines(self):
-        for i in range(10):
+        for i in range(12):
             x = randint(0, 9)
             y = randint(0, 9)
-            self.grid[x][y] = 100           # TODO: Use an array of objects instead of integers? 
-                                            # Mines are set as 100 since proximity from last revealed tile can't reach that far
-
+            if (not self.grid[x][y].mine):
+                self.grid[x][y].mine = True 
+    
+    # Reveals mines (used in debug)
     def revealGrid(self, grid):
         for x in range(10):
             for y in range(10):
-                if (grid[x][y] == 100):
-                    grid[x][y] = 101
+                if (grid[x][y].mine):
+                    grid[x][y].visible = True
 
     def getGrid(self):
         return self.grid
 
+    # Checks whether or not the tile is in bounds
+    # Fixed the out of bounds error because it went by list index, not grid size
+    # Grid is 9x9, and the erroneous tiles had indexes of 10
+    def inboundChecker(self, x, y):
+        if (x >= 0 and x < 10 and y >= 0 and y < 10):
+            return True
+        return False
+
+    # Checks if user has hit a mine
+    def isMineHit(self, grid, x, y):
+        if (grid[x][y].mine):
+            return True
+        return False
+
+    # TODO: This is better in an if-else.. right? I can't remember
+    # TODO: That or I have frog brain
+    # Searches the adjacent tiles of the clicked tile for mines
+    def searchAdjMine(self, grid, x, y):
+        adj = 0
+        # Search adj north
+        if (self.inboundChecker(x - 1, y)):
+            if (self.isMineHit(grid, x - 1, y)):
+                adj += 1
+
+        # Search adj south
+        if (self.inboundChecker(x + 1, y)):
+            if (self.isMineHit(grid, x + 1, y)):
+                adj += 1
+
+        # Search adj west
+        if (self.inboundChecker(x, y - 1)):
+            if (self.isMineHit(grid, x, y - 1)):
+                adj += 1
+
+        # Search adj east
+        if (self.inboundChecker(x, y + 1)):
+            if (self.isMineHit(grid, x, y + 1)):
+                adj += 1
+
+        # Search north-east
+        if (self.inboundChecker(x - 1, y + 1)):
+            if (self.isMineHit(grid, x - 1, y + 1)):
+                adj += 1
+
+        # Search north-west
+        if (self.inboundChecker(x - 1, y - 1)):
+            if (self.isMineHit(grid, x - 1, y - 1)):
+                adj += 1
+
+        # Search south-east
+        if (self.inboundChecker(x + 1, y + 1)):
+            if (self.isMineHit(grid, x + 1, y + 1)):
+                adj += 1
+
+        # Search south-west
+        if (self.inboundChecker(x + 1, y - 1)):
+            if (self.isMineHit(grid, x + 1, y - 1)):
+                adj += 1
+
+        return adj 
+
+    # Searches neighboring tiles for any blanks and fill 
+    def search(self, grid, x, y):
+
+        if (not self.inboundChecker(x, y)):
+            return
+
+        tile = grid[x][y]
+
+        if (tile.visible):
+            return
+
+        if (tile.mine):
+            return 
+
+        numMines = self.searchAdjMine(grid, x, y)
+        tile.visible = True
+
+        if numMines > 0:
+            grid[x][y].numAdj = numMines
+            return
+
+        # Do recursion to search through more tiles 
+        self.search(grid, x, y + 1)
+        self.search(grid, x, y - 1)
+        self.search(grid, x + 1, y)
+        self.search(grid, x - 1, y)
+        
+        
 Board.makeGrid(Board)
 Board.makeMines(Board)
 
