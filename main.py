e378631b76a0987714ef671bf89aebd9eb787e4b
penjikenji <buitien28@gmail.com>
2021-03-13 19:04:08 -0500

    Added images and used Tile objects instead of ints

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 ######-General TODOs-######
-# TODO: Be able to reveal adjacent blank tiles upon clicking on a tile
-# TODO: ^^^ Show numbers on revealed tiles ^^^ (How did i forget this)
-# TODO: These classes are still unorganized 
-# TODO: Replace drawn tiles with assets from WinXP game (May just use pygame-gui to show a separate window with Microsoft BOB)
-# TODO: ^^^ Microsoft BOB will be the indicator for win or game over ^^^
-# TODO: Menubar for reset/difficulty/etc. (May require either pygame-gui or pygame-menu)
+# TODO: Add smiley for win/lose/reset indicator
+# TODO: Add time and flag counter???
 # TODO: Cleaning up the mess
 # TODO: Beat minesweeper
 ######---------------######
-# TODO Line 65: Be able to uncheck a flagged block
 # TODO Line 111: Implement a debug mode
 ######-Suggestions-########
-# Use objects instead of ints
 # Limited number of flags (An array of counters that only reaches up to 10? Then when it is unchecked it is removed using pop())
 # Un-hard code everything
 # Minimize size of tiles/resolution
+# Menubar for reset/difficulty/etc. (May require either pygame-gui or pygame-menu)
 
 import pygame
+import os
 from random import randint
 
 from board import Board
@@ -24,24 +20,26 @@ from tile import Tile
 import time
 
 # mmmm constants
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
 GRAY = (140, 138, 137)
-RED = (255, 0, 0)
-PURPLE = (167, 66, 245)
 
 class Main():
-    def __init__(self, size = [250, 250], width = 24, height = 24, margin = 1):
+    def __init__(self, size = [200, 250], width = 19, height = 19, margin = 1):
 
-        self.size = size        # There's an "error" here because sonarlint says I'm not following code conventions (python:S5717)             
-        self.width = width      # Shut up sonarlint i can do whatever i want
+        self.size = size                     
+        self.width = width      
         self.height = height
         self.margin = margin
+
+        # Find size of each tile for images
+        # Load images into a dictionary
+        self.tileSize = (self.size[0] // 10, 200 // 10)
+        self.images = {}
 
         self.grid = []
 
         self.screen = pygame.display.set_mode(self.size)
 
+        self.flagCounter = 10
         self.mined = False
         self.isWin = False
 
@@ -51,14 +49,15 @@ class Main():
     def run(self):
         pygame.init()
         running = True
-        pygame.display.set_caption("Blindsweeper")  # It's called Blindsweeper because you can't see past a block and they aren't numbered (yet)
+        pygame.display.set_caption("Pymine") # It's not blind anymore yay
         pygame.display.set_icon(pygame.image.load('resources/bomb.png'))
-        self.grid = Board.getGrid(Board)            # I think there's a better way to implement this and it starts with inheritence
+        self.grid = Board.getGrid(Board)  
+        self.loadImage()          
         self.draw()
 
         if (self.isDebug == True):
            self.debug()
-
+           
         while running:
             for event in pygame.event.get():
                 if (event.type == pygame.QUIT):
@@ -69,30 +68,40 @@ class Main():
                     x = Tile().mouseClickX(self.width, self.margin)
                     y = Tile().mouseClickY(self.height, self.margin)
 
-                    # TODO: be able to uncheck a flagged tile (might be time to switch to using objects instead of ints)
-                    # If the user right clicks, it flags the tile
-                    # continue is used becase mine does not need to be checked
-                    if (event.button == 3):
-                        self.grid[x][y] = 200
+                    # Ignore any input outside of grid 
+                    if (not Board().inboundChecker(x, y)):
                         continue
 
-                    # Returns true if there is a mine beneath the revealed tile
-                    if (Tile().isMineHit(self.grid, x, y)):
-                        self.grid[x][y] = 101
-                        self.mined = True
-                    else:
-                        self.grid[x][y] = 1
-                        Tile().searchAdjMine(self.grid)
+                    # Tile is flagged/unflagged when user right clicks on tile
+                    # continue is used becase mine does not need to be checked
+                    if (event.button == 3 and self.grid[x][y].flagged == False and self.flagCounter > 0):
+                        self.grid[x][y].flagged = True
+                        self.flagCounter -= 1
+                        continue
 
-                
-            
+                    if (event.button == 3 and self.grid[x][y].flagged):
+                        self.grid[x][y].flagged = False
+                        self.flagCounter += 1
+
+                    if (event.button == 1):
+
+                        # Returns true if there is a mine beneath the revealed tile
+                        if (Board().isMineHit(self.grid, x, y)):
+                            self.grid[x][y].visible = True
+                            self.mined = True
+                            continue
+                        else:
+
+                            #Searches for tiles with adjacent mines and does a flood fill(?) of tiles with no adjacent mines
+                            Board().search(self.grid, x, y)
+                    
             # Closes the game after a delay of 2 seconds when won
-            if (self.isGridCleared() == True):
+            if (self.isGridCleared()):
                 time.sleep(2)
                 running = False
 
             # Reveals the mines and closes after a delay of 5 seconds
-            elif (self.mined == True):
+            elif (self.mined):
                 Board().revealGrid(self.grid) 
                 self.draw()
                 pygame.display.flip()
@@ -104,33 +113,33 @@ class Main():
             clock = pygame.time.Clock()
             clock.tick(60)
             pygame.display.flip()
+
         pygame.quit()
 
     # Draws the grid and colors in tiles for user input
     def draw(self):
-        self.screen.fill(BLACK)
+        tLeft = (0, 0) # Used to append an image onto each tile using coordinates
+        self.screen.fill(GRAY)
         for x in range(10):
             for y in range(10):
-                color = WHITE
-                if self.grid[y][x] == 1:
-                    color = GRAY
-                elif self.grid[y][x] == 101:
-                    color = RED
-                elif self.grid[y][x] == 200:
-                    color = PURPLE
 
-                # This makes all the tiles
-                pygame.draw.rect(self.screen, color, [(self.margin + self.width) * y + self.margin, 
-                                                    (self.margin + self.height) * x + self.margin, 
-                                                    self.height, self.width])
+                # Will apply a different image depending on tile state
+                image = self.images[self.getTileImg(self.grid[y][x])] 
+                self.screen.blit(image, tLeft)
+
+                tLeft = (tLeft[0] + self.tileSize[0], tLeft[1])
+
+            tLeft = (0, tLeft[1] + self.tileSize[1])
 
     # Debug mode which reveals mines and user input in the terminal, including an option to reload a new grid
     # TODO: Actually implement this better (Do a reveal all?)
     def debug(self):
         for x in range(10):
             for y in range(10):
-                if (self.grid[x][y] == 100):
+                if (self.grid[x][y].mine):
                     print(f"Mines are placed at {x} {y}")
+            Board().revealGrid(self.grid)
+            self.draw()
 
     # Resets the game, typically when a game is won or lost
     def reset(self):
@@ -142,10 +151,36 @@ class Main():
     def isGridCleared(self):
         for x in range(10):
             for y in range(10):
-                if (self.grid[x][y] == 0):
+                if (not self.grid[x][y].visible):
                     return False
         return True
 
+    # Loads images from resources folder into a dict
+    def loadImage(self):
+        for file in os.listdir("resources"):
+            if (not file.endswith(".png")):
+                continue
+            image = pygame.image.load(r"resources/" + file)
+            image = pygame.transform.scale(image, self.tileSize)
+            self.images[file.split(".")[0]] = image
+
+    # Gets the image from dict depending on tile state
+    def getTileImg(self, tile):
+        if (tile.mine and tile.visible):
+            return "bomb"
+        
+        if (not tile.mine and tile.visible and tile.numAdj == 0):
+            return "blankblock"
+       
+        if (tile.flagged):
+            return "flag"
+        
+        if (tile.visible and tile.numAdj > 0):
+            return f"block{tile.numAdj}"
+
+        return "block"
+    
 minesweeper = Main()
 minesweeper.run()
+
 
